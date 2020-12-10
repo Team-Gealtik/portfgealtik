b015a12575c9481be103fa68d6b541c500a7c6a3
Seperkopian Jakarta <awetsemoga38@gmail.com>
2020-12-11 02:01:28 +0700

    Databases: Now I'm using postgrest in local and fix the problem

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -115,8 +115,6 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -152,7 +150,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # static files
