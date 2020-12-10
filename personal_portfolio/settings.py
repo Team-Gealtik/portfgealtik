2d9ebb09ea5d1bbcae017c8eea9ad64e69167fde
Seperkopian Jakarta <awetsemoga38@gmail.com>
2020-12-11 01:52:07 +0700

    Databases: Now I'm using postgrest in local and fix the problem

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # static files
 STATIC_URL = '/static/'
